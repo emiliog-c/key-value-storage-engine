5d0c366eeca0f84004d3a8621bc1794461e725ce
emiliog-c <51037944+emiliog-c@users.noreply.github.com>
2026-04-09 00:16:16 +1000

    added a basic lsm tree

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+import json
+
 from fastapi import FastAPI, HTTPException
 from fastapi.testclient import TestClient
 from pydantic import BaseModel
 
+memory_db = {}
+sst_num = 1
 app = FastAPI()
 
 
@@ -9,7 +13,8 @@ class Key(BaseModel):
     value: str
 
 
-memory_db = {}
+with open("manifest.txt", "w") as fp:
+    pass
 
 
 @app.get("/")
@@ -19,12 +24,33 @@ def read_root():
 
 @app.get("/{key}")
 def read_item(key: str):
+    global memory_db
+    print(f"this is {memory_db}")
     if key in memory_db:
+        print("woop")
         return {"value": memory_db[key], "key": key}
-    raise HTTPException(status_code=404, detail="Item not found")
+    else:
+        with open("manifest.txt", "r") as fp:
+            for sstable in reversed(fp.readlines()):
+                with open(sstable.strip(), "r") as d:
+                    temp_db = json.load(d)
+                if key in temp_db:
+                    return {"value": temp_db[key], "key": key}
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
 @app.put("/{key}")
 def update_item(key: str, item: Key):
+    global sst_num
     memory_db[key] = item.value
+    new_sstable = f"sstable_{sst_num}.json"
+    if len(list(memory_db.keys())) > 3:
+        with open(new_sstable, "w") as d:
+            json.dump(memory_db, d)
+            d.close()
+        with open("manifest.txt", "a") as d:
+            d.write(new_sstable + "\n")
+            d.close()
+        sst_num += 1
+        memory_db.clear()
     return {"value": item.value, "key": key}
